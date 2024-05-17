6df1711146518b8ef712b464174835e8041ed41d
adrisala20 <adriana_15_tuya@hotmail.com>
2024-05-17 17:39:06 +0200

    added link with php

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'blog')
+
+@section('content')
+<main>
+    <div class="container">
+        <h2>
+            Questo é il mio primo collegamento 
+        </h2>
+    </div>  
+</main>
+
+@endsection
